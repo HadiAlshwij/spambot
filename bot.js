0ba8a217e0cd7a6e55551b0b038fd396d8df4b87
HadiAlshwij <57157186+HadiAlshwij@users.noreply.github.com>
2019-10-29 22:45:51 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,645 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const myid = ['535450365672554506'];
+const prefix = ['.'];
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+client2.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client3.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client4.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client5.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client6.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client7.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client8.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client9.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client10.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client11.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client12.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client13.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client14.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client15.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client16.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client17.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client18.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client19.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client20.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client1.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+client5.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client6.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client7.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client8.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client9.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client11.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client12.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client13.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client14.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client15.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client16.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client17.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client18.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client19.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client20.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**credit please**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client1.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client2.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client3.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client4.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client5.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client6.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client7.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client8.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client9.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client10.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client21.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client12.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client13.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client14.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client15.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client16.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client17.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client18.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client19.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+client20.on('message', message => {
+    
+    if(message.content.startsWith('say')) {
+if(message.author.id != "535450365672554506") return message.reply('ERROR');
+
+      const args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.reply('**type the message**')             
+        message.channel.send(args)
+    }
+  })
+
+
+
+
+
+
+
+
+
+client.login(process.env.token);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7);
+client8.login(process.env.TOKEN8);
+client9.login(process.env.TOKEN9);
+client10.login(process.env.TOKEN10);
+client11.login(process.env.TOKEN11);
+client12.login(process.env.TOKEN12);
+client13.login(process.env.TOKEN13);
+client14.login(process.env.TOKEN14);
+client15.login(process.env.TOKEN15);
+client16.login(process.env.TOKEN16);
+client17.login(process.env.TOKEN17);
+client18.login(process.env.TOKEN18);
+client19.login(process.env.TOKEN19);
+client20.login(process.env.TOKEN20);
